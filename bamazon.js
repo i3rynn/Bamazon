ce3ad437a95423fb926b6dbae164347fdd469875
Brynn Chapman <55081531+i3rynn@users.noreply.github.com>
2019-12-22 01:42:56 -0600

    Add files via upload

---
+++ bamazon.js
@@ -0,0 +1,65 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3300,
+    user: "root",
+    password: "Silver*001",
+    database: "bamazon"
+})
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connection succesful!");
+    makeTable();
+})
+
+var makeTable = function () {
+    connection.query("SELECT * FROM products", function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].itemid + " || " + res[i].productname + " || " + res[i].departmentname + " || " + res[i].price + " || " + res[i].stockquantity + "\n");
+        }
+        promptCustomer(res);
+    })
+}
+
+var promptCustomer = function (res) {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'choice',
+        message: "What would you like to purchase? [Quit with Q]"
+    }]).then(function (answer) {
+        varcorrect = false;
+        for (var i = 0; i < res.length; i++) {
+            if (res[i].productname == answer.choice) {
+                correct = true;
+                var product = answer.choice;
+                var id = i;
+                inquirer.prompt({
+                    type: "input",
+                    name: "quant",
+                    message: "How many would you like to buy?",
+                    validate: function (value) {
+                        if (isNaN(value) == false) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }).then(function (answer) {
+                    if ((res[id].stockquantity-answer.quant)>0){
+                        connection.query("UPDATE products SET stockquantity=' "+(res[id].stockquantity-answer.quant)+" 'WHERE productname='"+product+"'",function(err,res2){
+                            console.log("Product Bought!");
+                            makeTable();
+                    })
+                }else{
+                    console.log("Not a valid selection!");
+                    promptCustomer(res);
+                }
+                })
+
+            }
+        }
+    })
+//paused at 6:45
